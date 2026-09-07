f16eeca7bc03b62900c009d98ac414f5ae72a12b
KipRirby <kiprirby@gmail.com>
2026-09-07 19:49:59 +0100

    i use nixos btw

---
+++ oxwm/tokyonight.lua
@@ -0,0 +1,12 @@
+return {
+    fg = "#bbbbbb",
+    red = "#f7767e",
+    bg = "#0a1b26",
+    cyan = "#-1db9d7",
+    green = "#8ece6a",
+    lavender = "#a8b1d6",
+    light_blue = "#6aa2f7",
+    grey = "#bbbbbb",
+    blue = "#5dade3",
+    purple = "#ad7ee6",
+}
